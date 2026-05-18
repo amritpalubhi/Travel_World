6a0948e409dd88beca5e8f91e204a75334c50be3
Amritpal Singh <amritubhi2002@gmail.com>
2026-05-18 17:41:41 +0530

    fixes bugs

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -87,6 +87,17 @@ const Home = () => {
     </Row>
   </Container>
 </section>
+<section>
+      <Container>
+        <Row>
+          <Col lg='12' className='mb-5'>
+            <Subtitle subtitle={'Explore'}/>
+            <h2 className='featured__tour-title'>Our Featured Tours</h2>
+          </Col>
+          <FeaturedTourList/>
+        </Row>
+      </Container>
+    </section>
     {/*===============featured tour section end=================*/}
 
     {/*===========gallery section starts============*/}
